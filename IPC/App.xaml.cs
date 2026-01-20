f08e008fb82555c407c578286df1621900e770d8
947413122 <gwq9474@outlook.com>
2026-01-20 15:03:17 +0800

    添加项目文件。

---
+++ IPC/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace IPC
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
